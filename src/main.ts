ddb6e4914b9c14fdc29758da179708c1182957e9
Rabin Samaroo <r1samaroo@ryerson.ca>
2022-01-18 03:52:43 +0000

    setup express

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import * as express from 'express';
+import * as cors from 'cors';
+
+const PORT = process.env.PORT || 8888;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`️[server] Server is running at http://localhost:${PORT}`);
+});
+
+module.exports = app; // for testing
