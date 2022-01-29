a59066448f516ebf4236bce7779b95070bcc4203
Rabin Samaroo <r1samaroo@ryerson.ca>
2022-01-29 21:55:55 +0000

    add db script

---
+++ utils/testDb.sql
@@ -0,0 +1,20 @@
+CREATE TABLE testtable (
+	id SERIAL PRIMARY KEY UNIQUE,
+	shortlink VARCHAR(32) NOT NULL UNIQUE,
+	longlink TEXT NOT NULL
+);
+
+DROP TABLE testtable;
+
+INSERT INTO testtable (shortlink, longlink)
+    VALUES ('foo', 'bar');
+
+INSERT INTO testtable (shortlink, longlink)
+    VALUES ('lel', 'kek');
+
+INSERT INTO testtable (shortlink, longlink)
+    VALUES ('bing', 'bong');
+
+SELECT * FROM testtable WHERE shortlink='bing';
+
+SELECT * FROM testtable;
